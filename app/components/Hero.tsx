cb3afaa10858a1a94a4bf089bda0ebf4dbdc7b2a
Toufique18 <toufiqueeamam7@gmail.com>
2026-04-17 16:49:33 +0600

    camel case fixed

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
 
             <button className="mt-8 px-6 py-3 bg-[#0F2B5A] text-white rounded-lg flex gap-1">
               Upload your resume
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
               </svg>
 
